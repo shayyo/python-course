229de3b7a31f9a63885ed373c4554fc8c9b16794
shayyo <shay.yosef@gmail.com>
2021-03-20 18:06:19 +0200

    Lessen 4 HM

--- fourth.py
+++ fourth.py
@@ -12,7 +12,7 @@ except:
     sys.exit(1)
 
 
-print("#### Exercise 2 ####")
+print("\n#### Exercise 2 ####")
 
 
 def count_word_occurrences(file):
@@ -30,3 +30,57 @@ def count_word_occurrences(file):
 
 
 print(count_word_occurrences(f_name))
+
+print("\n#### Exercise 3 ####")
+
+with open(f_name) as fh:
+    for i in range(2):  # read first two lines
+        print(fh.readline())
+
+
+print("\n#### Exercise 4 ####")
+
+t = "this is a text to be checked for the longest word in it"
+text = t.split()
+longest = 0
+for w in text:
+    if len(w) > longest:
+        longest = len(w)
+
+print(f"The longest word in text is {longest} character long")
+
+
+print("\n#### Exercise 5 ####")
+
+t = "This is a text to be reversed"
+
+print(t[::-1])
+
+
+print("\n#### Exercise 6 ####")
+
+
+class MyClass:
+
+    def get_string(self, string):
+        self.print_string(string)
+
+    def print_string(self, string):
+        print(string.upper())
+
+
+obj = MyClass()
+obj.get_string('A class object')
+
+
+print("\n#### Exercise 7 ####")
+
+
+class Rectangle:
+
+    def calc_rectangle(self, length, width):
+        print(f"Results is: {length * width}")
+
+
+obj = Rectangle()
+obj.calc_rectangle(5, 55)
