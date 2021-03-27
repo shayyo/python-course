a1829a042816f294d23b3c931a4fbb435eeeffda
shayyo <shay.yosef@gmail.com>
2021-03-27 15:29:53 +0300

    project1 first commit

--- fourth.py
+++ fourth.py
@@ -84,3 +84,37 @@ class Rectangle:
 
 obj = Rectangle()
 obj.calc_rectangle(5, 55)
+
+
+print("\n#### Exercise 8 ####")
+
+open_sigh = ["[", "(", "{"]
+close_sign = ["]", ")", "}"]
+open_s = {}
+close_s = {}
+stack = []
+
+t = "this is ]["
+
+open_list = ["[", "{", "("]
+close_list = ["]", "}", ")"]
+
+
+# Function to check parentheses
+def check(myStr):
+    stack = []
+    for i in myStr:
+        if i in open_list:
+            stack.append(i)
+        elif i in close_list:
+            pos = close_list.index(i)
+            if ((len(stack) > 0) and
+                    (open_list[pos] == stack[len(stack) - 1])):
+                stack.pop()
+            else:
+                return "Unbalanced"
+    if len(stack) == 0:
+        return "Balanced"
+    else:
+        return "Unbalanced"
+print(check(t))
