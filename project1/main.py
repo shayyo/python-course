1d23eb5dc16be9a71d926ebc67b21585edd93983
shayyo <shay.yosef@gmail.com>
2021-03-28 18:02:48 +0300

    project1 first commit

--- project1/main.py
+++ project1/main.py
@@ -4,22 +4,28 @@ file_name = "encrypted.txt"
 with open(file_name) as f:
     encrypted_text = f.read().lower()
 
-print(encrypted_text)
-
 letters_to_occurrences = {}
-counted_letter = []
-
 for i in encrypted_text:
     if not i.isalpha():
         continue
-    if i == " ":
-        continue
-    if i not in counted_letter:
-        num = encrypted_text.count(i)
-        n = {i: num}
-        letters_to_occurrences.update(n)
-        counted_letter.append(i)
+    letters_to_occurrences[i] = letters_to_occurrences.get(i, 0) + 1
 
-print(letters_to_occurrences)
+letters_to_occurrences_sorted = dict(reversed(sorted(letters_to_occurrences.items(), key=lambda item: item[1])))
+most_common_enc_str = list(letters_to_occurrences_sorted)[:4]
+most_common_english = ['e', 't', 'o', 'f']
+most_common = {}
+for i in range(4):
+    n = {most_common_english[i]: most_common_enc_str[i]}
+    m = {most_common_enc_str[i]: most_common_english[i]}
+    most_common.update(n)
+    most_common.update(m)
 
+################################################
+unencrypted_text = ""
+for i in encrypted_text:
+    if i in most_common:
+        unencrypted_text += most_common[i]
+    else:
+        unencrypted_text += i
 
+print(unencrypted_text)
